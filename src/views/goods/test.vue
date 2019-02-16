4eb037037c93c4864160b2cea6515b9af5b0cec9
Benson.Cai <3230758810@qq.com>
2019-02-16 17:57:50 +0800

    first commit

---
+++ src/views/goods/test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="huil">
+        hiashiwqhk
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus">
+    .huil{
+        font-size: 15px;
+    }
+</style>
